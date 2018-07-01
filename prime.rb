a95724a29672c0c00a62110a503f490675ae2ed0
Ana Bebic <anabebic.x@gmail.com>
2018-07-01 13:53:21 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,15 +3,8 @@ def prime?(n)
    if (n % d) == 0
     return false
    end
-  end
+  
 
   true
  end
 
- def is_prime2 n
-  foundDivider = false
-   for d in 2..(n - 1)
-    foundDivider = ((n % d) == 0) or foundDivider
-   end
-  not foundDivider
- end
