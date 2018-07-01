1f9c37f88ca2a1900c64c63f0b78cff2f600e35a
Ana Bebic <anabebic.x@gmail.com>
2018-07-01 13:57:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,22 @@
-def prime?(n)
-  for d in 2..(n - 1)
-   if (n % d) == 0
-    return false
-   end
-  
+# Add  code here!
+require 'benchmark'
+require 'bigdecimal/math'
 
+iterations = 10
+
+def prime?(integer)
+  return false if integer <= 1
+  Math.sqrt(integer).to_i.downto(2).each do |divisor|
+    return false if integer % divisor == 0
+  end
   true
- end
+end
+
+def prime_wo_sqrt?(integer)
+  return false if integer <= 1
+  worstcase = integer - 1
+  worstcase.downto(2).each do |divisor|
+    return false if integer % divisor == 0
+  end
+  true
 end
